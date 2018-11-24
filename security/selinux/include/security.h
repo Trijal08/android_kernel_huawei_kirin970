7ec6aa63dbcf6063003a0426d736cbc6c631e29c
Alexey Dobriyan <adobriyan@gmail.com>
2018-11-24 12:11:32 +0300

    UPSTREAM: selinux: make "selinux_policycap_names[]" const char *
    Those strings aren't written.
    
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    
    (cherry picked from commit 89f5bebcf0401dac470756869587a50dd72ff7b5)
    Change-Id: I968b2f6197b2d749b3786089b942655d1880258a
    Bug: 140252993
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -83,6 +83,13 @@ enum {
 
 extern const char *selinux_policycap_names[__POLICYDB_CAPABILITY_MAX];
 
+extern int selinux_policycap_netpeer;
+extern int selinux_policycap_openperm;
+extern int selinux_policycap_extsockclass;
+extern int selinux_policycap_alwaysnetwork;
+extern int selinux_policycap_cgroupseclabel;
+extern int selinux_policycap_nnp_nosuid_transition;
+
 /*
  * type_datum properties
  * available at the kernel policy version >= POLICYDB_VERSION_BOUNDARY
