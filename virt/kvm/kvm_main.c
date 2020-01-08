fa2b0173c9caec20b86f4891c5b53dc95dcd3c2f
Sean Christopherson <sean.j.christopherson@intel.com>
2020-01-08 12:24:37 -0800

    KVM: Use vcpu-specific gva->hva translation when querying host page size
    [ Upstream commit f9b84e19221efc5f493156ee0329df3142085f28 ]
    
    Use kvm_vcpu_gfn_to_hva() when retrieving the host page size so that the
    correct set of memslots is used when handling x86 page faults in SMM.
    
    Fixes: 54bf36aac520 ("KVM: x86: use vcpu-specific functions to read/write/translate GFNs")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1297,7 +1297,7 @@ unsigned long kvm_host_page_size(struct kvm_vcpu *vcpu, gfn_t gfn)
 
 	size = PAGE_SIZE;
 
-	addr = kvm_vcpu_gfn_to_hva_prot(vcpu, gfn, NULL);
+	addr = kvm_vcpu_gfn_to_hva(vcpu, gfn);
 	if (kvm_is_error_hva(addr))
 		return PAGE_SIZE;
 
