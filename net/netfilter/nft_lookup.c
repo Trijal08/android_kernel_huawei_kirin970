cdee7dea9057847de4ff4eb07ce71bca7ebeab51
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2026-01-07 17:33:28 -0500

    Revert "netfilter: nf_tables: allow lookups in dynamic sets"
    This reverts commit 8353df98dcb779106b086ee9fa368e73f2261b2c.

--- net/netfilter/nft_lookup.c
+++ net/netfilter/nft_lookup.c
@@ -76,8 +76,8 @@ static int nft_lookup_init(const struct nft_ctx *ctx,
 	if (IS_ERR(set))
 		return PTR_ERR(set);
 
-	priv->sreg = nft_parse_register(tb[NFTA_LOOKUP_SREG]);
-	err = nft_validate_register_load(priv->sreg, set->klen);
+	err = nft_parse_register_load(tb[NFTA_LOOKUP_SREG], &priv->sreg,
+				      set->klen);
 	if (err < 0)
 		return err;
 
